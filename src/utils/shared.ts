af6c1cf71898e735fa10f2ab57d590ce5e566b55
Kingsley Amoke <klordbravo@gmail.com>
2024-10-11 11:04:02 +0100

    minor update

--- src/utils/shared.ts
+++ src/utils/shared.ts
@@ -1,65 +1,66 @@
-
-import Toast from 'react-native-root-toast'
+import Toast from "react-native-root-toast";
 
 export const actions = [
-    {
-      id:1,
-      name: 'Buy Data',
-      icon: "signal-cellular-alt",
-      link: 'data'
-    },
-    {
-      id:2,
-      name: 'Airtime Topup',
-      icon: "add-call",
-      link:'airtime'
-    },
-    {
-      id:3,
-      name: 'Aitime 2 Cash',
-      icon: 'payment',
-      link:'airtime-2-cash'
-    },
-    {
-      id:4,
-      name: 'Cable Sub',
-      icon: "live-tv",
-      link:'cable'
-    },
-    {
-      id:5,
-      name: 'Electricity SUb',
-      icon: "electric-bolt",
-      link:'electricity'
-    },
-    {
-      id:6,
-      name: 'Scratch Card',
-      icon: "airplane-ticket",
-      link:'scratch-card'
-    },
-    
-  ]
+  {
+    id: 1,
+    name: "Buy Data",
+    icon: "signal-cellular-alt",
+    link: "data",
+  },
+  {
+    id: 2,
+    name: "Airtime Topup",
+    icon: "add-call",
+    link: "airtime",
+  },
+  {
+    id: 3,
+    name: "Aitime 2 Cash",
+    icon: "payment",
+    link: "airtime-2-cash",
+  },
+  {
+    id: 4,
+    name: "Cable Sub",
+    icon: "live-tv",
+    link: "cable",
+  },
+  {
+    id: 5,
+    name: "Electricity Sub",
+    icon: "electric-bolt",
+    link: "electricity",
+  },
+  {
+    id: 6,
+    name: "Scratch Card",
+    icon: "airplane-ticket",
+    link: "scratch-card",
+  },
+];
 
-  //toast
+//toast
 
-  export const CustomToast = (message: string, bgColor:string, textColor:string) => {
-
-    return (Toast.show(message, {
-      duration: Toast.durations.LONG,
-      position: Toast.positions.TOP,
-      shadow: true,
-      animation: true,
+export const CustomToast = (
+  message: string,
+  bgColor: string,
+  textColor: string
+) => {
+  return Toast.show(message, {
+    duration: Toast.durations.LONG,
+    position: Toast.positions.TOP,
+    shadow: true,
+    animation: true,
     hideOnPress: true,
     delay: 0,
     backgroundColor: bgColor,
     textColor: textColor,
-    textStyle:{
+    textStyle: {
       fontSize: 16,
-      fontWeight: 'bold'
+      fontWeight: "bold",
     },
     containerStyle: {
-      marginTop:70
-    }
-  }))
-  }
+      marginTop: 70,
+    },
+  });
+};
